f25d6539917e5971a7794244f56cdfea863df004
ktsukamoto1985 <tsukamoto.kohtaroh@swing-w.com>
2020-05-29 11:42:30 +0000

    finish ch06

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
- validates :name,  presence: true,
+  before_save { self.email = self.email.downcase }
+  validates :name,  presence: true,
                    length: { maximum: 30 }
- VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- validates :email, presence: true,
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
                    length: { maximum: 255 },
                    format: { with: VALID_EMAIL_REGEX },
-                   uniqueness: { case_sensitive: false }
+                   uniqueness: true
+  has_secure_password
+  
+  validates :password, presence: true, length: { minimum: 6 }
 end
