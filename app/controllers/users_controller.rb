828d5658da1c0e8a3fcc3986f68c1ef28b6f8697
ktsukamoto1985 <tsukamoto.kohtaroh@swing-w.com>
2020-05-30 06:01:44 +0000

    finished 7.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
   
+  # GET /users/:id
   def show
     # @user = User.first
     @user = User.find(params[:id])
   end
   
+  # GET /users/new
   def new
+    @user = User.new
+  end
+  
+  # POST /users (+ params)
+  def create
+    # User.create(params[:user])
+    @user = User.new(user_params)
+    if @user.save
+      # Success (valid params)
+    else
+      # Failure (invalid params)
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 end
