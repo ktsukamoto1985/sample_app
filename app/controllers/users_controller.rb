9e681aebbfadbc03b937b7165f0b8a0640746c27
ktsukamoto1985 <tsukamoto.kohtaroh@swing-w.com>
2020-06-10 13:30:36 +0000

    finished ch10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
-  # ログインしていないとindexアクション、editアクション、updateアクションは使えない
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  # ログインしていないとindexアクション、editアクション、updateアクション、destroyアクションは使えない
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   # 正しいユーザーじゃないとeditアクション、updateアクションは使えない
   before_action :correct_user,   only: [:edit, :update]
+  # 管理者じゃないとdestroyできない
+  before_action :admin_user,     only: :destroy
   
   # GET /users/
   def index
@@ -65,6 +67,13 @@ class UsersController < ApplicationController
     end
   end
   
+  # DELETE /users/:id
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
   
   
   private
@@ -87,6 +96,11 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+  
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
   
 end
