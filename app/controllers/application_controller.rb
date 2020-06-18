b151be26fbdfb103dbb917a6b928a17de595ea0d
ktsukamoto1985 <tsukamoto.kohtaroh@swing-w.com>
2020-06-18 13:17:18 +0000

    13.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   # sessions_controllerもusers_controllerもApplicationControllerを継承しているので、
   # ここに便利メソッドを入れておくと全部の子Controllerにも継承できる
   include SessionsHelper
+  
+  private
 
-  # def hello
-  #   render html: "hi!"
-  # end
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,11 @@ class User < ApplicationRecord
   # User.new.digest('foobar')
   # みたいになる
   
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   
 end
