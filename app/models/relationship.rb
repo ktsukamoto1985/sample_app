bd5b9a1896af85a1805f223e106b3a57796eb9c9
ktsukamoto1985 <tsukamoto.kohtaroh@swing-w.com>
2020-06-20 06:02:26 +0000

    14.1

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,4 +4,7 @@ class Relationship < ApplicationRecord
   
   belongs_to :followed, class_name: "User"
   # 規約だと、"modelname_id" => followed_idになってFollowedクラスを探すため、わざわざクラス名を指定する
+  
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
